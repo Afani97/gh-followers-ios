c2ae470123373e129ce0a31ee55f8780593eabb2
Aristotel Fani <afani1997@gmail.com>
2020-02-15 15:59:32 -0500

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Aristotel Fani on 2/15/20.
+//  Copyright © 2020 Aristotel Fani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
